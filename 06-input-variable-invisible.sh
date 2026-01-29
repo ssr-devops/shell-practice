ffbfe20ac2ba737cebff71f61e36b32a97ab3555
ssr-devops <surendar,dev08@gmail.com>
2026-01-29 13:44:07 +0530

    with special(arguments) variables

--- 06-input-variable-invisible.sh
+++ 06-input-variable-invisible.sh
@@ -9,3 +9,13 @@ echo "Logging in with Username: $USERNAME"
 echo "Logging in with Password: $PASSWORD"
 # Note: For security reasons, we do not display the password
 echo "Password entered successfully."
+
+
+sleep 5
+echo "The name of the script is : $0"
+echo "The first parameter is : $1"
+echo "The second parameter is : $2"
+echo "The total number of parameters passed is : $#"
+echo "All the parameters passed are : $*"
+echo "The process ID of the current script is : $$"
+echo "The last executed command's exit status is : $?"
