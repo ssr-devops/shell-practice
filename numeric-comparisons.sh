a713107b3cd4acb0a638abd3e0e4f9a600de2203
ssr-devops <surendar,dev08@gmail.com>
2026-01-29 20:59:05 +0530

    enter age(-ge) and validation

--- numeric-comparisons.sh
+++ numeric-comparisons.sh
@@ -3,7 +3,7 @@
 echo "Please enter your age: "
 read age
 
-if [[ $age -gt 18 ]]; then
+if [[ $age -ge 18 ]]; then
     echo "You are eligible to vote."
 else
     echo "You are not eligible to vote."
