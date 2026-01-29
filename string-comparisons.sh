4b4fcdf7da193ca9b6c200f00658696ef6c7fecf
ssr-devops <surendar,dev08@gmail.com>
2026-01-29 21:18:05 +0530

    string comparisons

---
+++ string-comparisons.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+NAME="Surendar"
+
+#== : Equals
+if [[ $NAME == "Surendar" ]]; then
+    echo "Hello, Surendar!"
+else
+    echo "Hello, stranger!"
+fi
+
+#!= : Not Equals
+if [[ $NAME != "Admin" ]]; then
+    echo "Access denied."
+else
+    echo "Welcome, Admin!"
+fi
+
+#-z : String is empty (zero length)
+if [[ $NAME < "Z" ]]; then
+    echo "$NAME comes before Z in the alphabet."
+else
+    echo "$NAME comes after Z in the alphabet."
+fi
+
+#-n : String is not empty (non-zero length)
+if [[ -n $NAME ]]; then
+    echo "The name variable is not empty."
+else
+    echo "The name variable is empty."
+fi
