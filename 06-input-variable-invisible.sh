88c68cbae04e90bc905af1dff2ed0192fa0bf6ad
ssr-devops <surendar,dev08@gmail.com>
2026-01-29 12:00:49 +0530

    input text(password) as varibales

--- 06-input-variable-invisible.sh
+++ 06-input-variable-invisible.sh
@@ -6,5 +6,6 @@ echo "Please enter the Password:"
 read -s PASSWORD
 
 echo "Logging in with Username: $USERNAME"
+echo "Logging in with Password: $PASSWORD"
 # Note: For security reasons, we do not display the password
 echo "Password entered successfully."
