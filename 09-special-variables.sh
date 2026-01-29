9b864e114048268c5a0918aa59cbd2fcaed8cd4d
ssr-devops <surendar,dev08@gmail.com>
2026-01-29 13:21:48 +0530

    special(defalt) variables

--- 09-special-variables.sh
+++ 09-special-variables.sh
@@ -2,6 +2,15 @@
 
 #### script to demonstrate special variables ####
 
+PERSON1=SureSH
+PERSON2=ramesh
+
+echo "$PERSON1 : Hello! How can I assist you today?"
+echo "$PERSON2 : Hi $PERSON1, Iam looking for a good book to read."
+echo "$PERSON1 : Sure! What genre are you interested in?"
+echo "$PERSON2 : I enjoy science fiction and fantasy."
+echo "$PERSON1 : Great choices! I recommend 'Dune' by Frank Herbert for science"
+
 echo "The name of the script is : $0"
 echo "The first argument is : $1"
 echo "The second argument is : $2"
