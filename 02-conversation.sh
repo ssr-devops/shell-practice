357d571e9b48bdc017594efbaf07039f9ae001a2
ssr-devops <surendar,dev08@gmail.com>
2026-01-29 10:44:03 +0530

    user conversaction

---
+++ 02-conversation.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+echo "Likky : Hello! How can I assist you today?"
+echo "chikky : Hi Likky, Iam looking for a good book to read."
+echo "Likky : Sure! What genre are you interested in?"
+echo "chikky : I enjoy science fiction and fantasy."
+echo "Likky : Great choices! I recommend 'Dune' by Frank Herbert for science"
+
