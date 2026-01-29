9fa8e07534c2ce98de927291d7ce77fdeedd3e57
ssr-devops <surendar,dev08@gmail.com>
2026-01-29 20:07:09 +0530

    read input value and check the condistion

--- read-input-value-conditions.sh
+++ read-input-value-conditions.sh
@@ -2,9 +2,9 @@
 
 read -p "Enter a nmber: " num
 
-if [[ $num -gt 0 ]]; then
+if [[ $num -gt 33 ]]; then
     echo "That's a big number!"
-    elif [[ $num -lt 0 ]]; then
+    elif [[ $num -lt 60 ]]; then
     echo "That's a small number!"
     else
     echo "You entered zero!"
