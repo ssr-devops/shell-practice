8c2fd95353350d72b8f51659aa86241ded90f893
ssr-devops <surendar,dev08@gmail.com>
2026-01-31 18:45:44 +0530

    packages installations

---
+++ pkg-installation.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+USERID=$(id -u)
+LOGS_DIR="/var/log/shell-practice"
+LOGS_FILE="/var/log/shell-practice/$0.log"
+
+if [ "$USERID" -ne 0 ]; then
+    echo "Please run as root" | tee -a "$LOGS_FILE"
+    exit 1
+fi
+
+mkdir -p "$LOGS_DIR"
+
+VALIDATE() {
+    if [ $1 -ne 0 ]; then
+        echo "$2 failed!" | tee -a "$LOGS_FILE"
+        exit 1
+    else
+        echo "$2 completed successfully." | tee -a "$LOGS_FILE"
+    fi
+}
+
+for pkg in $@ # sudo <file name> nginx mysql nodejs
+do
+    dnf install -y "$pkg" &>> "$LOGS_FILE"
+    VALIDATE $? "$pkg installation"
+done
